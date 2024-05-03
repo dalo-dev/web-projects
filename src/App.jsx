a9627edc90311d8590374f126fb16408c381353b
DALO <dalodev@proton.me>
2024-05-03 15:58:44 -0500

    Organize folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import ProjectsList from "./components/ProjectsList";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ProjectsList from "./components/ProjectsList/ProjectsList";
 
 function App() {
   return (
--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Project from "./Project/Project";
+import Project from "../Project/Project";
 
 const gridStyles = {};
 
