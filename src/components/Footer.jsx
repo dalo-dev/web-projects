09870550947e7f5e027797086268eded214d59a5
DALO <dalodev@proton.me>
2024-05-03 10:13:11 -0500

    Added header and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Made with love by{" "}
+        <a href="https://github.com/dalo-dev" target="_blank">
+          dalo-dev
+        </a>
+      </p>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Web projects</h1>
+      <p>
+        This webpage is a showcase for all my web development personal projects,
+        neatly organized and easily accessible, reflecting my skills and
+        creativity in the digital realm.
+      </p>
+    </header>
+  );
+}
